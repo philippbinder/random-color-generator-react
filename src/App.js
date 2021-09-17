8ac8a7071ef293cc63b6c3d3238b08afd123dfb4
Philipp Binder <binder.philipp10@gmail.com>
2021-09-17 17:59:50 +0200

    Add more styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { useState } from 'react';
 
 const randomColor = require('randomcolor');
@@ -12,50 +11,78 @@ function App() {
   return (
     <div
       style={{
-        width: 600,
-        height: 300,
-        border: '10px solid black',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        marginTop: 100,
+        backgroundColor: 'black',
+        marginTop: 0,
       }}
     >
-      <h1> Random Color Generator - React </h1>
-      <p>
-        {' '}
-        {
-          `I am ${divcolor}. If you don't like me, click me away.` /* prints out the hex code, backticks make plain text a string literal and the $ enables me to search the code, so with the {} I am able to find the const that contains the random */
-        }{' '}
-      </p>
       <div
         style={{
-          // display: 'flex',
-          // flexWrap: 'wrap',
-          height: 'auto',
-          width: 'auto',
-          backgroundColor: `${divcolor}` /*  backticks to search for a variable from outside and inside the enitre code for the state variable that changes (or only the function since it's not globally defined?)  */,
+          width: 600,
+          height: 300,
+          border: '10px solid grey',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          marginTop: 100,
+          backgroundColor: 'yellow',
         }}
       >
-        {' '}
-        I know I suck, but so does this button.{' '}
-      </div>
-      <button
-        style={
+        <h1
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          {' '}
+          Surprise! Another Random Color Generator!!!{' '}
+        </h1>
+        <h2
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          {' '}
+          Waste your time and match the colors!{' '}
+        </h2>
+        <p
+          style={{
+            textAlign: 'center',
+          }}
+        >
+          {' '}
           {
-            // margipaddnLeft: 100,
-            // marginRight: 'auto',
-            // not working, want to move button to the center
+            `I am ${divcolor}. Click until the matching color appears.` /* prints out the hex code, backticks make plain text a string literal and the $ enables me to search the code, so with the {} I am able to find the const that contains the random */
+          }{' '}
+        </p>
+        <div
+          style={{
+            // display: 'flex',
+            // flexWrap: 'wrap',
+            // textAlign: 'center',
+            height: 40,
+            width: 'auto',
+            backgroundColor: `${divcolor}` /*  backticks to search for a variable from outside and inside the enitre code for the state variable that changes (or only the function since it's not globally defined?)  */,
+          }}
+        >
+          {' '}
+          I know I suck, but so does this button.{' '}
+        </div>
+        <button
+          style={
+            {
+              // margipaddnLeft: 100,
+              // marginRight: 'auto',
+              // not working, want to move button to the center
+            }
           }
-        }
-        onClick={() =>
-          setDivcolor(
-            randomColor() /* onClick triggers on Click and the arrow functions tells it to fire up the setDivcolor function from the state variable which contains the random color. randomColor() is taken from the documentation of it's library and runs it's function / method. */,
-          )
-        }
-      >
-        {' '}
-        Generate Random Color{' '}
-      </button>
+          onClick={() =>
+            setDivcolor(
+              randomColor() /* onClick triggers on Click and the arrow functions tells it to fire up the setDivcolor function from the state variable which contains the random color. randomColor() is taken from the documentation of it's library and runs it's function / method. */,
+            )
+          }
+        >
+          {' '}
+          Generate Random Color{' '}
+        </button>
+      </div>
     </div>
   );
 }
